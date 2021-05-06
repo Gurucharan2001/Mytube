fcceb6a70673ce26d5ef66982c0bb4795776a708
Gurucharan2001 <togmaparu@gmail.com>
2021-05-06 09:09:46 +0530

    Remove extra comments

--- src/App.js
+++ src/App.js
@@ -13,34 +13,7 @@ const App = () => {
       <a href="#">MyTube</a>
     </header>
     <SearchArea></SearchArea>
-    {/* <Video title="The First Vedio in MyTube" 
-    dateAdded="4 days ago" 
-    channel= "Your Channel"  />
-    <Video title="The Cool Video" 
-    dateAdded="4 days ago" 
-    channel= "GoldsMine"  />
-    <Video title="Motu Patlu session:4" 
-    dateAdded="2 days ago" 
-    channel= "Your Cartoon"  /> */}
   </div>)
 
-  // return React.createElement("div", {}, [
-  //   React.createElement("h2", {}, "MyTube"),
-  //   React.createElement(video, {
-  //     title: "The First Vedio in MyTube",
-  //     dateAdded: "4 days ago",
-  //     channel: "Your Channel",
-  //   }),
-  //   React.createElement(video, {
-  //     title: "The Cool Video",
-  //     dateAdded: "2 years ago",
-  //     channel: "GoldsMine",
-  //   }),
-  //   React.createElement(video, {
-  //     title: "Motu Patlu session:4",
-  //     dateAdded: "2 hours ago",
-  //     channel: "Your Cartoon",
-  //   }),
-  // ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
